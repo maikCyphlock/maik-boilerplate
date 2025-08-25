b65ac453b240c2c4a56f02b91dd5f0cfa2aebbd9
maikCyphlock <maikolaguilar656@gmail.com>
2025-08-25 11:49:43 -0400

    feat(layout): actualizar fuentes y agregar proveedor de React Query
    Se modificó el archivo `layout.tsx` para incluir las fuentes `Jost` y `Nunito`, y se reemplazó el uso de las fuentes anteriores. Además, se integró el `ReactQueryProvider` para gestionar el estado de las consultas en la aplicación.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Jost, Nunito } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+import { ReactQueryProvider } from '@/lib/react-query';
+const JostFont = Jost({
+  variable: "--font-jost",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const NunitoFont = Nunito({
+  variable: "--font-nunito",
   subsets: ["latin"],
 });
 
@@ -25,9 +25,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${JostFont.variable} ${NunitoFont.variable} antialiased`}
       >
+        <ReactQueryProvider>
         {children}
+        </ReactQueryProvider>
       </body>
     </html>
   );
