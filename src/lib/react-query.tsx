c11df32971c20669ef8b0f475f9af5963cdcdc38
maikCyphlock <maikolaguilar656@gmail.com>
2025-08-26 14:30:40 -0400

    feat(react-query): actualizar configuración de QueryClient para mejorar manejo de mutaciones y agregar tiempo de recolección de basura
    Se ha modificado la configuración del `QueryClient` en el archivo `react-query.tsx`. Se ha añadido un nuevo parámetro `gcTime` de 5 minutos y se ha cambiado la opción de reintento de mutaciones a `false`, lo que optimiza el comportamiento de las consultas y mutaciones en la aplicación.

--- src/lib/react-query.tsx
+++ src/lib/react-query.tsx
@@ -3,16 +3,18 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { type ReactNode, useState } from "react";
-
-const QueryConfiguration = {
+import type { QueryClientConfig } from "@tanstack/react-query";
+const QueryConfiguration:QueryClientConfig = {
 	defaultOptions: {
 		queries: {
 			refetchOnWindowFocus: false,
 			retry: 2,
 			staleTime: 1000 * 60, // 1 minuto
+			gcTime: 5 * 60 * 1000, // 5 minutos
+			
 		},
 		mutations: {
-			retry: 1,
+			retry: false, // No se puede hacer retry de mutations
 		},
 	},
 };
