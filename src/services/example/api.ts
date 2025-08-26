32d686bc268d3cecc70bba7056e0e3fe0211d584
maikCyphlock <maikolaguilar656@gmail.com>
2025-08-26 11:44:15 -0400

    refactor(api): simplificar la estructura de las funciones getTodos y addTodo
    Se ha mejorado la legibilidad del archivo `api.ts` al simplificar la estructura de las funciones `getTodos` y `addTodo`, eliminando espacios innecesarios y asegurando una mejor organización del código.

--- src/services/example/api.ts
+++ src/services/example/api.ts
@@ -3,12 +3,12 @@ import { api } from "../api.conf";
 
 // GET: obtener todos los todos
 export async function getTodos() {
-    const { data } = await api.get("/todos");
-    return data;
-  }
-  
-  // POST: crear un nuevo todo
-  export async function addTodo(todo: { text: string }) {
-    const { data } = await api.post("/todos", todo);
-    return data;
-  }
+  const { data } = await api.get("/todos");
+  return data;
+}
+
+// POST: crear un nuevo todo
+export async function addTodo(todo: { text: string }) {
+  const { data } = await api.post("/todos", todo);
+  return data;
+}
